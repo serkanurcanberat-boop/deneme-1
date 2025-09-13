a1be9f647b1e437a411138da629bdcbefdad5cc4
xllargee_123 <serkanurcanberat@gmail.com>
2025-09-13 15:42:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 javascript
 console.log("JavaScript dosyası başarıyla bağlandı!");
-
+```js
+// Firebase config (BURAYI KENDİ FİREBASE BİLGİLERİNLE DOLDUR)
+const firebaseConfig = {
+  apiKey: "API_KEYİN",
+  authDomain: "PROJE.firebaseapp.com",
+  projectId: "PROJE_ID",
+  storageBucket: "PROJE.appspot.com",
